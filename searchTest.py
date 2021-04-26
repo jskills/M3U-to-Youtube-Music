6bf32a1a7c04ae98c100ae1f33eb0913c8dd513e
JSkills <jskills@gmail.com>
2021-04-26 19:57:10 -0400

    different use cases

--- searchTest.py
+++ searchTest.py
@@ -3,8 +3,6 @@
 from ytmusicapi.ytmusic import YTMusic
 
 
-### authenticate w Youtube Music
-
 ytm = YTMusic('headers_auth.json')
 
 searchList = list()
@@ -12,23 +10,15 @@ searchList = list()
 searchList.append("Lee Moses - Hey Joe")
 searchList.append("Joe Bataan - CALL MY NAME")
 searchList.append("Creep")
+searchList.append("H.E.R. - Fight For You")
 searchList.append("Ugly Casanova - Barnacles")
 
 
 for searchFor in searchList:
    
-    songDict = list()
-
     print("Querying YTMusic for : " + searchFor)
     songDict = ytm.search(searchFor, 'uploads', limit=40)
-    try: 
-        songDict = ytm.search(searchFor, 'uploads', limit=40)
-    except:
-        print("Failed search for : " + searchFor) 
 
     print("Result : ")
     print(str(songDict))
 
-
-
-###################
