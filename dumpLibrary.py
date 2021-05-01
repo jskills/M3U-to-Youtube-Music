895ce91e82ad5931a4795ea1029b6cb32570dbd7
JSkills <jskills@gmail.com>
2021-05-01 13:00:47 -0400

    initial commit

---
+++ dumpLibrary.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from ytmusicapi.ytmusic import YTMusic
+
+songLimit = 10
+
+ytm = YTMusic('headers_auth.json')
+
+songList = ytm.get_library_upload_songs(limit=songLimit)
+
+for sl in songList:
+    print(str(sl))
